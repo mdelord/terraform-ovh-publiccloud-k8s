1fb6f9cc98ba8065f427cfc3a1ccb52a9025704c
Yann DEGAT <yann.degat@gmail.com>
2018-03-04 18:01:30 +0100

    Fix broken build after etcd upgraded dep

--- main.tf
+++ main.tf
@@ -207,7 +207,7 @@ resource "openstack_compute_instance_v2" "singlenet_k8s" {
 
 module "post_install_cfssl" {
   source  = "ovh/publiccloud-cfssl/ovh//modules/install-cfssl"
-  version = ">= 0.1.2"
+  version = ">= 0.1.3"
 
   count                   = "${var.post_install_modules && var.cfssl && var.cfssl_endpoint == "" && var.count >= 1 ? 1 : 0}"
   triggers                = ["${element(concat(openstack_compute_instance_v2.singlenet_k8s.*.id, openstack_compute_instance_v2.multinet_k8s.*.id), 0)}"]
@@ -219,7 +219,7 @@ module "post_install_cfssl" {
 
 module "post_install_etcd" {
   source  = "ovh/publiccloud-etcd/ovh//modules/install-etcd"
-  version = ">= 0.1.0"
+  version = ">= 0.1.1"
 
   count                   = "${var.post_install_modules && var.etcd ? var.count : 0}"
   triggers                = ["${concat(openstack_compute_instance_v2.singlenet_k8s.*.id, openstack_compute_instance_v2.multinet_k8s.*.id)}"]
