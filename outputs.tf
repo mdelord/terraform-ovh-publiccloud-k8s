5b68c6b3604a8d2468fb8aea145519c923fc5a82
Yann DEGAT <yann.degat@gmail.com>
2018-03-21 10:03:17 +0100

    Fix api_endpoint output with workers

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-locals {
-  k8s_admin_config = "/etc/kubernetes/admin.conf"
-
-  etcd_test_command = <<CMD
-/opt/etcd/bin/etcdctl \
-    --ca-file /opt/etcd/certs/ca.pem \
-    --cert-file /opt/etcd/certs/cert.pem \
-    --key-file /opt/etcd/certs/cert-key.pem \
-    --endpoints ${module.userdata.etcd_endpoints} \
-    member list \
-CMD
-
-  k8s_test_command = <<CMD
-sudo /opt/k8s/bin/kubectl \
-    --kubeconfig ${local.k8s_admin_config} \
-    get nodes \
-CMD
-=======
-output "public_security_group_id" {
-  value = "${join("", openstack_networking_secgroup_v2.pub.*.id)}"
->>>>>>> Rework tests and associated outputs
-}
-
 output "private_ipv4_addrs" {
   value = ["${data.template_file.ipv4_addrs.*.rendered}"]
 }
@@ -47,6 +23,6 @@ output "etcd_endpoints" {
 
 output "api_endpoint" {
   # TODO: replace this by a DNS entry to round robin on masters IP
-  description = "This represents the public k8s api endpoint"
-  value       = "${var.api_endpoint != "" ? var.api_endpoint : (var.master_mode ? format("%s:6443", element(concat(data.template_file.public_ipv4_addrs.*.rendered, list("")), 0)) : "")}"
+  description = "This represents the public k8s api endpoint only if `master_mode` is set to true"
+  value       = "${var.master_mode ? format("%s:6443", element(concat(data.template_file.public_ipv4_addrs.*.rendered, list("")), 0)) : ""}"
 }
