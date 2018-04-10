516d7582dc266afa991615e38d012d3d08fdc353
Yann DEGAT <yann.degat@gmail.com>
2018-04-10 10:06:34 +0200

    Rework test: add containers multihost ping test

--- test/test_ssh_public.tf
+++ test/test_ssh_public.tf
@@ -1,3 +1,15 @@
 locals {
   test_ssh_prefix = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no core@${module.k8s.public_ipv4_addrs[0]} --"
 }
+
+resource "null_resource" "test" {
+  connection {
+    host                = "${module.k8s.public_ipv4_addrs[0]}"
+    user                = "core"
+  }
+
+  provisioner "file" {
+    content = "${data.template_file.test_script.rendered}"
+    destination = "/tmp/test.sh"
+  }
+}
