5cb887e124cf0bcea44eb257c29ca20cbf1d2a96
Yann DEGAT <yann.degat@gmail.com>
2018-04-18 14:44:27 +0200

    Rework travis setup

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 REGIONS=${1:-$OS_REGION_NAME}
-#DIRS=(public-cluster-cl private-cluster-cl public-cluster-with-workers-cl)
-DIRS=(public-cluster-with-workers-cl)
+PUBLISH=$2
+BASEDIR="$(dirname $0)/.."
+
+DIRS=(public-cluster-cl public-cluster-cl-prebuilt private-cluster-cl public-cluster-with-workers-cl)
+
+# build packer image
+(cd examples/k8s-glance-image && make coreos)
 
 if [ ! -f "$SSH_AUTH_SOCK" ]; then
     eval $(ssh-agent) && ssh-add ${TEST_SSH_PRIVATE_KEY:-$HOME/.ssh/id_rsa}
@@ -10,9 +15,14 @@ fi
 
 for d in ${DIRS[@]}; do
     for r in $REGIONS; do
-        if ! $(dirname $0)/runtest.sh "$(dirname $0)/../examples/$d" "$r"; then
+        if ! "$BASEDIR/test/runtest.sh" "$BASEDIR/examples/$d" "$r"; then
             echo "last test failed. skipping nexts."
             exit 1
         fi
     done
 done
+
+if [ "$PUBLISH" == "publish" ]; then
+    # if everything is ok, publish
+    (cd "$BASEDIR/examples/k8s-glance-image" && make publish-coreos)
+fi
---
+++ test/travis_setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+BASEDIR="$(dirname $0)/.."
+
+if [ ! -f "$BASEDIR/.ovhtravis.gpg" ]; then
+    echo "Missing ovhtravis gpg key, won't be able to sign files" >&2
+else
+    gpg --import "$BASEDIR/.ovhtravis.gpg"
+fi
+
+if [ -z "$GPG_PASS" ]; then
+    echo "Missing gpg passphrase, signing files may fail" >&2
+else
+    echo $GPG_PASS > "$BASEDIR/.gpg.passphrase"
+fi
+
+echo "install jq & curl" >&2
+sudo apt-get -qq update
+sudo apt-get install -y jq curl
+
+echo "install openstack & swift clis" >&2
+sudo pip install python-openstackclient python-swiftclient
+
+echo "creating HOME/bin directory" >&2
+mkdir -p ~/bin
+export PATH="~/bin:$PATH"
+
+echo "installing terraform" >&2
+curl -sLo terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip terraform.zip
+mv terraform ~/bin
+
+echo "installing terraform" >&2
+curl -sLo packer.zip https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+unzip packer.zip
+mv packer ~/bin/packer-io
+
+echo "generating keypair for test purposes" >&2
+ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+
